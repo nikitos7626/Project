418ab7c8a1ecde04e9d8862c1b35338547b60add
etewwwclimber@gmail.com <etewwwclimber@gmail.com>
2023-11-23 00:50:21 +0300

    жопа

--- UP/Form1.cs
+++ UP/Form1.cs
@@ -33,11 +33,11 @@ namespace UP
 
         }
 
-        private void textBox1_TextChanged(object sender, EventArgs e)
+        private void textBox1_TextChanged(object sender, EventArgs e)//анальный секс
         {
 
         }
-        private void textBox2_TextChanged(object sender, EventArgs e)
+        private void textBox2_TextChanged(object sender, EventArgs e)//жопа пкпререрн66
         {
 
         }
