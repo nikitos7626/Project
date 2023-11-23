fcfa2a72e1d43c3d0968ccc4c08ea31e85a89a82
etewwwclimber@gmail.com <etewwwclimber@gmail.com>
2023-11-23 15:23:08 +0300

    каталог

---
+++ UP/Catatlog.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace UP
+{
+    partial class Catatlog
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonAddBalance = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxAddBalance = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.labelBalance = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // buttonAddBalance
+            // 
+            this.buttonAddBalance.Location = new System.Drawing.Point(116, 140);
+            this.buttonAddBalance.Name = "buttonAddBalance";
+            this.buttonAddBalance.Size = new System.Drawing.Size(152, 54);
+            this.buttonAddBalance.TabIndex = 0;
+            this.buttonAddBalance.Text = "Пополнить баланс";
+            this.buttonAddBalance.UseVisualStyleBackColor = true;
+            this.buttonAddBalance.Click += new System.EventHandler(this.buttonAddBalance_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 92);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(129, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Пополнить баланс";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // textBoxAddBalance
+            // 
+            this.textBoxAddBalance.Location = new System.Drawing.Point(147, 89);
+            this.textBoxAddBalance.Name = "textBoxAddBalance";
+            this.textBoxAddBalance.Size = new System.Drawing.Size(100, 22);
+            this.textBoxAddBalance.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(253, 95);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(18, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "р.";
+            this.label2.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(47, 47);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(58, 16);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Баланс:";
+            this.label3.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // labelBalance
+            // 
+            this.labelBalance.AutoSize = true;
+            this.labelBalance.Location = new System.Drawing.Point(111, 47);
+            this.labelBalance.Name = "labelBalance";
+            this.labelBalance.Size = new System.Drawing.Size(35, 16);
+            this.labelBalance.TabIndex = 1;
+            this.labelBalance.Text = "1000";
+            this.labelBalance.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(144, 47);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(15, 16);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "р";
+            this.label5.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Каталог
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBoxAddBalance);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.labelBalance);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonAddBalance);
+            this.Name = "Каталог";
+            this.Text = "Каталог";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonAddBalance;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxAddBalance;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label labelBalance;
+        private System.Windows.Forms.Label label5;
+    }
+}
---
+++ UP/Catatlog.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UP
+{
+    public partial class Catatlog : Form
+    {
+        public Catatlog()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonAddBalance_Click(object sender, EventArgs e)
+        {
+            int balance = Convert.ToInt32(labelBalance.Text);
+
+            balance += Convert.ToInt32(textBoxAddBalance.Text);
+
+            labelBalance.Text = balance.ToString();
+        }
+    }
+}
